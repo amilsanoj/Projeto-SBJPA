6427533057dcb9f0b2533e64231e1b56b30eb62c
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-08-07 11:23:03 -0300

    INIT

---
+++ src/main/java/com/studies/projetosbjpa/ProjetoSbjpaApplication.java
@@ -0,0 +1,13 @@
+package com.studies.projetosbjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSbjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSbjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studies/projetosbjpa/ProjetoSbjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.projetosbjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
