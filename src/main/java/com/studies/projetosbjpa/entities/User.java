7d2c8b02ac8132861f416724ce89065914331ccb
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-08-07 11:52:32 -0300

    H2 test

--- src/main/java/com/studies/projetosbjpa/entities/User.java
+++ src/main/java/com/studies/projetosbjpa/entities/User.java
@@ -1,11 +1,17 @@
 package com.studies.projetosbjpa.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -21,6 +27,7 @@ public class User implements Serializable {
     }
 
     public User() {
+
     }
 
     public Long getId() {
