92d92b175832461dd5ef27b1e2ca5bc7c9146c16
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-08-26 16:19:53 -0300

    OrderStatus Implements

--- src/main/java/com/studies/projetosbjpa/entities/Order.java
+++ src/main/java/com/studies/projetosbjpa/entities/Order.java
@@ -1,7 +1,7 @@
 package com.studies.projetosbjpa.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.studies.projetosbjpa.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
@@ -22,13 +22,16 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -66,5 +69,15 @@ public class Order {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }
